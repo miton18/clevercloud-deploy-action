28ac9811bae0314eafdc5a2f57f6a8f9f8a939cb
miton18 <remi@collignon-ducret.fr>
2023-07-21 16:36:56 +0200

    init

---
+++ clevercloud.client.d.ts
@@ -0,0 +1,7 @@
+declare module '@clevercloud/client/esm/request.superagent.js'
+
+declare module '@clevercloud/client/esm/api/v2/application.js'
+
+declare module '@clevercloud/client/esm/oauth.browser.js'
+
+declare module '@clevercloud/client/esm/prefix-url.js'
